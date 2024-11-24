521a52390c8442a4f1590376bc18db5b614777ce
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-11-24 07:13:04 +0200

    prevent app from rotate

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:gpa_culator/gpa_calculator.dart';
-
+import 'package:flutter/services.dart';
 import 'core/services/sql_lite_services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   AppData.initialDB();
+  Future.wait([
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]),
+  ]);
   runApp(const GpaCalculatorApp());
 }
